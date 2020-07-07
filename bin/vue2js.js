aa4ca48b8f6c49ab3ae7b6378716f35abf7ec44b
toyobayashi <lifenglin314@outlook.com>
2020-07-07 12:46:01 +0800

    2

--- bin/vue2js.js
+++ bin/vue2js.js
@@ -3,8 +3,12 @@
 const path = require('path')
 const fs = require('fs')
 
-const file = process.argv.slice(2)[0]
+const files = process.argv.slice(2)
 const { Parser } = require('..')
-const result = new Parser().parse(file)
-fs.writeFileSync(path.join(path.dirname(file), path.parse(file).name + '.js'), result.js, 'utf8')
-fs.writeFileSync(path.join(path.dirname(file), path.parse(file).name + '.css'), result.css, 'utf8')
+const parser = new Parser()
+for (let i = 0; i < files.length; i++) {
+  const file = files[i]
+  const result = parser.parseFile(file)
+  fs.writeFileSync(path.join(path.dirname(file), path.parse(file).name + '.js'), result.js, 'utf8')
+  fs.writeFileSync(path.join(path.dirname(file), path.parse(file).name + '.css'), result.css, 'utf8')
+}
